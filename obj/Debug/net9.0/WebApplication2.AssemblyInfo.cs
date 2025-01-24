7a62fd12335069689ea13286b75d061dd83bc4a8
Darrenfisherlol <darren.nov@gmail.com>
2025-01-23 22:19:01 -0600

    Create menu controller and view

--- obj/Debug/net9.0/WebApplication2.AssemblyInfo.cs
+++ obj/Debug/net9.0/WebApplication2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f5cd0b4a3f47f6f429a5a7331b25e878fea5a7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f20106756b2eddfac4eff2858b53216f3d8129d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
