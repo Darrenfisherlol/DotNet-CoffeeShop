826a70c7c75dd62e0ce5a382483a0bd04a2f6250
Darrenfisherlol <darren.nov@gmail.com>
2025-02-21 20:28:45 -0600

    coffee and home view standardization

--- obj/Debug/net9.0/WebApplication2.AssemblyInfo.cs
+++ obj/Debug/net9.0/WebApplication2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e98f54e4d88b7fb27752c9c73e5f9418718753c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+277d0ddc46fad73939994550964abd80ceba3a15")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
